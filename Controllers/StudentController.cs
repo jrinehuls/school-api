538b9d50c3a754fdcc139e8aee5765a3ef102797
Justin Rinehuls <jrinehuls@gmail.com>
2024-04-19 19:51:31 -0400

    Add student model and controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SchoolAPI.Models;
+
+namespace SchoolAPI.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("All")]
+        public ActionResult<List<Student>> getStudents()
+        {
+            List<Student> students = new List<Student>
+            {
+                 new Student {Id = 1, FirstName = "Jamal", LastName = "Bascome"}
+            };
+
+            return Ok(students);
+        }
+    }
+}
