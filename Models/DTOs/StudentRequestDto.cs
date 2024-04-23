d914d06af8716f28a0b2a463a6968e35b550e81f
Justin Rinehuls <jrinehuls@gmail.com>
2024-04-22 22:34:46 -0400

    Add comments for remiders in future

--- Models/DTOs/StudentRequestDto.cs
+++ Models/DTOs/StudentRequestDto.cs
@@ -4,16 +4,19 @@ namespace SchoolAPI.Models.DTOs
 {
     public class StudentRequestDto
     {
+        // Allowing null is fine here because validation will
+        // pick up on null, but be required, sending meaningful
+        // response on fileds to correct
         [Required(AllowEmptyStrings = false)]
-        public required string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        public required string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [EmailAddress]
-        public required string Email { get; set; }
+        public string? Email { get; set; }
 
         [Required(AllowEmptyStrings = false)]
-        public required string Address { get; set; }
+        public string? Address { get; set; }
     }
 }
