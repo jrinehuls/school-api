3ec9d8904a16c6b776ae1a3499723b842bf13065
Justin Rinehuls <jrinehuls@gmail.com>
2024-04-23 22:34:11 -0400

    Add ef tools and configure connection string

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SchoolAPI.Data;
 using SchoolAPI.Services;
 using SchoolAPI.Services.Impl;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
